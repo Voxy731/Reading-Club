c68bf2dd4c50bd627bb26c43efaef19333e6f0c8
Voxy731 <voxyblast@gmail.com>
2026-09-02 18:32:28 +0700

    Add files via upload
    update the website

---
+++ buku.js
@@ -0,0 +1,22 @@
+const daftarBukuDigital = {
+    wowo: {
+        judul: "Si Anak Badai",
+        sampul: "sianakbadai.jpg",
+        linkPdf: "Si Anak Badai - Tere Liye.pdf",
+    },
+    laskar: {
+        judul: "Laskar Pelangi",
+        sampul: "laskar_pelangi.jpg",
+        linkPdf: "Laskar Pelangi - Andrea Hirata.pdf", 
+    },
+    krisiskebebasan: {
+        judul: "Krisis Kebebasan",
+        sampul: "krisiskepercayaan.jpg",
+        linkPdf: "Krisis Kebebasan - Albert Camus.pdf"
+    },
+    duniashopie: {
+        judul: "Dunia Shople",
+        sampul: "duniashopie.jpg",
+        linkPdf: "Dunia Sophie (Jostein Gaarder).pdf"
+    }
+};
---
+++ isi-artikel.js
@@ -0,0 +1,86 @@
+const daftarBuku = {
+    wowo: {
+        judul: "Kisah wowo dan sawit",
+        isi: `alkisah, di sebuah negri konoha,
+tinggalah seseorang bernama wowo,
+dia sangat suka sawit...
+
+
+ya haha gak semua mungkin tapi dia baru saja menanam sawit di halaman belakang rumahnya`
+    },
+    laskar: {
+        judul: "Perkembangan Teknologi Di Era Digital",
+        isi: `
+Belakangan ini teknologi terus berkembang dengan sangat pesat. Teknologi membawa inovasi yang mengubah cara kita hidup, bekerja, dan berkomunikasi . Era digital saat ini menjadi saksi perkembangan teknologi terbaru yang membawa dampak yang sangat signifikan dalam berbagai aspek kehidupan. Banyak sekali orang didunia ini yang memanfaatkan perkembangan ini seperti sekolah-sekolah yang sudah mulai menggunakan laptop dalam kegiatan ajar-mengajar setiap harinya.
+
+Teknologi terbaru masa kini membawa kita menuju era transformasi digital yang memukau. Dari kemajuan dalam kecerdasan buatan yang memungkinkan mesin untuk belajar dan beradaptasi, hingga jaringan 5G yang menghadirkan konektivitas supercepat dan dapat diandalkan, inovasi ini mempengaruhi setiap aspek kehidupan kita. Internet of Things (IoT) memungkinkan perangkat untuk berkomunikasi dan berbagi data secara langsung, menciptakan lingkungan yang terhubung dan efisien. Selain itu, teknologi biometrik, seperti pengenalan wajah dan sidik jari, telah memperkuat lapisan keamanan di berbagai sektor. Sementara itu, realitas virtual (VR) dan augmented reality (AR) memberikan pengalaman yang mendalam dan interaktif, merubah cara kita belajar, bermain, dan bekerja. Kemudian ada teknologi yang bernama Quantum computing yaitu teknologi komputasi yang berpotensi menghadirkan komputer yang sangat kuat, mampu menyelesaikan masalah yang sulit sekalipun dengan hitungan detik. Dalam bidang sains, kesehatan, dan keuangan, quantum computing dapat mengakselerasi penemuan dan inovasi.
+
+Dengan kita menerapkan teknologi dalam kehidupan, dapat mempermudah kita sebagai manusia untuk melakukan aktivitas setiap harinya. Teknologi tidak akan pernah mati sampai kapanpun. Teknologi terus berkembang setiap hari, setiap bulan, setiap tahun, setiap saat. Segala sesuatu pada jaman sekarang pasti akan melibatkan yang namanya teknologi terumata dalam bidang digital. Teknologi mempunyai kelebihan dan kekurangannya masing-masing. Manfaat teknologi ini akan sangat bermanfaat bagi banyak orang, diantaranya adalah mengembangkan lapangan kerja baru, memperlancar komunikasi, mempermudah proses jual beli, memperluas wawasan,
+
+sumber informasi, hiburan, dan masih banyak lagi hal yang bisa dikembangkan oleh teknologi. Kita bisa lihat bermacam-macam teknologi yang telah tersebar, antara lain dalm bidang teknologi transportasi, teknologi komunikasi, teknologi informasi, teknologi kedokteran, teknologi pendidikan, dan teknik sipil.
+
+Pada ujungnya kita semua akan terpengaruh dengan teknologi yang akan mendatang, karena teknologi digital adalah teknologi yang sangat melekat kepada manusia. Teknologi digital sangat berpengaruh besar bagi kita Masyarakat. Dapat dimanfaatkan sebagai sarana perdagangan yang menjadi salah satu kunci kehidupan. Di era digital ini akan sangat mudah untuk melakukan kegiatan jual beli, sudah banyak toko online yang menyebar pada platform yang terpercaya. Bisnis pada era digital sudah sangat berkembang dari jaman ke jaman, kita dapat mempromosikan produk kita dengan gampang dan tidak perlu membayar dengan harga yang sangat tinggi. Dengan teknologi ini pun transaksi jual beli menjadi sangat mudah yaitu kita dapat menggukan uang elektronik, yang bisa dilakukan kapan saja dan Dimana pun kita berada.
+
+Semua inovasi ini bersama-sama membentuk lanskap teknologi yang dinamis dan menjanjikan, mempersiapkan kita untuk tantangan masa depan yang semakin kompleks. Dapat disimpulkan bahwa kita sebagai manusia tidak bisa terlepas dengan yang Namanya teknologi terutama dalam bidang teknologi digital. Teknologi digital sangat banyak telah mempengaruhi kita semua, yang menimbulkan sifat ketergantungan di masa depan. Teknologi akan terus berkembang sampai kapanpun karena teknologi akan membantu pekerjaan manusia menjadi lebih efisien dan lebih praktis.
+
+SC : Binus University`
+    },
+    mong: {
+        judul: "Mong Mong",
+        isi: "Ini isi cerita Mong Mong."
+    },
+
+    ufo: {
+        judul : "UFO Dan Alien Di Antara Kita",
+        isi : `UFO dan Alien di Antara Kita
+
+Pada tahun 1940-an dan 50-an, laporan tentang "piring terbang" menjadi fenomena budaya Amerika. Penampakan benda-benda aneh di langit menjadi bahan mentah bagi Hollywood untuk menyajikan visi tentang potensi ancaman. Poster film, seperti Earth vs. the Flying Saucers dari tahun 1956, menggambarkan ketakutan ini. Terkait dengan gagasan yang terus berkembang tentang kehidupan di Bulan, kanal-kanal di Mars, dan gagasan tentang peradaban Mars, piring terbang telah menjadi representasi harapan dan ketakutan dunia modern.
+
+
+
+Apakah para pengunjung dari dunia lain yang diduga itu damai dan baik hati, ataukah mereka akan menyerang dan menghancurkan umat manusia? Kekuatan penghancur bom atom mempertanyakan potensi kemajuan teknologi . Ketakutan akan kemungkinan kehancuran di era Perang Dingin terbukti menjadi lahan subur bagi kecemasan di bumi untuk mewujudkan visi piring terbang dan pengunjung dari dunia lain yang mungkin bersembunyi di antara kita di depan mata.
+
+
+
+Alien di Antara Kita dan Ketakutan terhadap Yang Lain
+
+Jika UFO mengunjungi dunia kita, di manakah makhluk luar angkasa ini? Mungkinkah mereka bersembunyi di antara kita? Buku komik dan televisi menggambarkan bagaimana kemungkinan kedatangan makhluk luar angkasa mencerminkan kecemasan pada era tersebut.
+
+
+
+Komik tahun 1962 berjudul There are Martians Among Us , dari Amazing Fantasy #15, menggambarkan bagaimana ketakutan terhadap makhluk luar angkasa dapat mencerminkan kecemasan Perang Dingin. Dalam komik tersebut, sebuah tim pencari berkumpul di sekitar pesawat alien yang mendarat, tetapi mereka tidak menemukan tanda-tanda keberadaan makhluk alien. Penyiar radio memperingatkan orang-orang di dekatnya untuk tetap berada di dalam rumah. Aksi kemudian beralih ke seorang suami dan istri saat sang suami bersiap untuk meninggalkan rumah mereka meskipun ada peringatan dari penyiar televisi untuk tetap berada di dalam rumah. Saat ia melambaikan tangan untuk mengucapkan selamat tinggal, ia mengingatkan istrinya untuk tetap di dalam. Namun, sang istri memutuskan untuk menyelinap keluar ke toko dan diserang serta diseret pergi. Sang suami kembali ke rumah dan mendapati rumahnya kosong, lalu berlari menuju telepon dengan panik. Dalam sebuah kejutan, suami yang cemas itu mengungkapkan bahwa dia dan istrinya adalah orang Mars.
+
+
+
+Ketakutan akan adanya musuh alien di tengah-tengah kita mengingatkan kita pada ketakutan terhadap Soviet dan komunis dari era McCarthy. Pada akhirnya, dalam cerita ini, manusialah yang mencegat dan menangkap wanita alien tersebut. Pergeseran perspektif ini menempatkan manusia pada posisi monster.
+
+
+
+UFO sebagai Cerita Rakyat Kontemporer
+
+Selain penggambaran UFO di media, UFO juga merupakan bagian dari budaya rakyat Amerika. Gagasan tentang alien dan piring terbang adalah bagian dari mitologi Amerika. Anda dapat menemukan dokumentasi pengalaman semacam ini dalam koleksi kehidupan rakyat. Sebuah wawancara dengan Howard Miller tentang berburu dan anjing pemburu, yang dikumpulkan sebagai bagian dari koleksi Tending the Commons: Folklife and Landscape in Southern West Virginia , mendokumentasikan pengalaman seseorang dengan potensi penampakan UFO.
+
+
+
+Dalam "Cahaya Misterius" , sebuah segmen dari wawancara etnografi, Miller menggambarkan cahaya aneh yang pernah dilihatnya saat berburu dengan anjing-anjingnya pada tahun 1966. "Tiba-tiba hari sudah terang, dan saya mendongak untuk melihat apa yang terjadi. Ada cahaya sebesar itu, naik, melayang ke atas bukit. Ketika saya melihat, cahaya itu langsung memudar. Saya pernah di Marinir, dan tahu seperti apa lampu pesawat, dan cahaya itu terlalu besar untuk pesawat." Ketika ditanya apakah dia tahu apa itu, dia menjawab, "Saya tidak tahu apa itu," tetapi kemudian menjelaskan, "Jika memang ada UFO, itulah yang terjadi." Cahaya yang tidak dapat dijelaskan saat berjalan di hutan ini merupakan contoh tipikal dari banyak kisah pertemuan semacam ini. Bukan hanya media yang menceritakan kisah dan mewakili ide-ide semacam ini, dokumentasi pengalaman dan cerita yang diceritakan orang Amerika satu sama lain juga sama pentingnya untuk memahami dan menafsirkan apa arti UFO bagi Amerika abad ke-20.
+
+
+
+Skeptisisme terhadap UFO dan Pertemuan dengan Alien
+
+Para ilmuwan dan astronom mengungkapkan berbagai tingkat antusiasme terhadap kemungkinan adanya kehidupan cerdas di alam semesta. Namun, para ilmuwan umumnya menolak gagasan bahwa ada alien yang mengunjungi Bumi. Dalam buku Pale Blue Dot: A Vision of the Human Future in Space , Carl Sagan mengulas kemungkinan pengunjung alien ke Bumi, dan menyarankan bahwa ada alasan yang kuat untuk skeptis terhadap mereka. Sebagian besar karya Sagan berfokus pada membongkar cerita rakyat dan kepercayaan serta mencoba mendorong pemikiran yang lebih teliti dan skeptis. Ia juga membahas kritik terhadap kepercayaan pada pengunjung alien dalam bukunya sebelumnya, Demon Haunted World: Science as a Candle in the Dark .
+
+
+
+Kritik yang begitu bersemangat terhadap kepercayaan pada UFO dari Sagan, yang terkenal dengan ide-ide spekulatifnya tentang kemungkinan peradaban alien, mungkin tampak sebagai sebuah kontradiksi. Sagan sendiri bahkan pernah berspekulasi tentang kemungkinan kunjungan alien kuno dalam esainya dari awal tahun 60-an yang berjudul " Kontak Langsung di antara Peradaban Galaksi melalui Penerbangan Antarbintang Relativistik" .
+
+
+
+Bagaimana kita mendamaikan Sagan yang skeptis dengan Sagan yang imajinatif? Jauh dari sebuah kontradiksi, kedua bagian dari perspektif Sagan ini menawarkan kerangka kerja untuk memahaminya dan pertukaran antara sains dan mitos tentang kehidupan di dunia lain. Skeptisisme dan imajinasi spekulatif menyatu sebagai dua bagian dari keseluruhan. Sangat penting untuk menerima dan mengeksplorasi ide-ide baru, betapapun anehnya, sambil pada saat yang sama menguji dan mengevaluasi validitas ide-ide tersebut.
+
+SC : Library Of Congress`
+    }
+
+};
+
+
